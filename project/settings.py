47d4d1b3eb2e29def7e0628c3d9c0f57627169b6
Ahmed Hatem <ahmedelrai18@gmail.com>
2020-06-23 06:51:43 +0200

    domain

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'eq9vwx6-&tk1=_&mb6a#78*06=nlb5v1kk*@qwlh54d-*y-cd_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['134.209.229.24']
+ALLOWED_HOSTS = ['134.209.229.24','elra3i.com']
 
 # Application definition
 
